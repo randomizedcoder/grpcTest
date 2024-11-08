8cba66d72de45950dfdfe5b11f7538f18a424ba4
randomizedcoder <dave.seddon.ca@gmail.com>
2024-11-07 19:54:28 -0800

    interceptors

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -25,47 +25,49 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/metadata"
 )
 
 var (
-	loops = flag.Int("loops", 10, "loops")
-	addr  = flag.String("addr", "localhost:50052", "the address to connect to")
-	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
-	retryPolicy = `{
-		"loadBalancingConfig": [{"round_robin":{}}],
-  		"timeout": "10.000000001s",
-		"methodConfig": [{
-		  "name": [{"service": "grpc.examples.echo.Echo"}],
-		  "retryPolicy": {
-			  "MaxAttempts": 1,
-			  "InitialBackoff": ".01s",
-			  "MaxBackoff": "3s",
-			  "BackoffMultiplier": 2.0,
-			  "RetryableStatusCodes": [
-				"DEADLINE_EXCEEDED",
-				"RESOURCE_EXHAUSTED",
-				"INTERNAL",
-				"UNAVAILABLE",
-				"DATA_LOSS"
-			  ]
-		  }
-		}]}`
+	loops       = flag.Int("loops", 10, "loops")
+	addr        = flag.String("addr", "localhost:50052", "the address to connect to")
+	policy      = flag.String("policy", "grpc_client_policy.yaml", "filename of the grpc client policy.yaml")
+	failpercent = flag.Int("failpercent", 50, "failpercent integers only between 0-100")
+	failcodes   = flag.String("failcodes", "4,8,14", "failcodes header to insert. single code, or comma seperated")
 )
 
 func main() {
 	flag.Parse()
 
+	ctx := context.Background()
+
+	// https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
+	// https://github.com/grpc/grpc-go/blob/11feb0a9afd8/examples/features/retry/client/main.go#L36
+	// https://grpc.github.io/grpc/core/md_doc_statuscodes.html
+	servicePolicyBytes, err := os.ReadFile(*policy)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Set up a connection to the server with service config and create the channel.
 	// However, the recommended approach is to fetch the retry configuration
 	// (which is part of the service config) from the name resolver rather than
 	// defining it on the client side.
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithDefaultServiceConfig(retryPolicy))
+	conn, err := grpc.NewClient(
+		*addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(string(servicePolicyBytes)),
+		grpc.WithUnaryInterceptor(unaryClientInterceptor(*failpercent, *failcodes)),
+	)
+
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -75,16 +77,34 @@ func main() {
 		}
 	}()
 
+	c := pb.NewEchoClient(conn)
 	for i := 0; i < *loops; i++ {
-		c := pb.NewEchoClient(conn)
 
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
 		defer cancel()
 
-		reply, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "Try and Success"})
+		reply, err := c.UnaryEcho(ctx,
+			&pb.EchoRequest{
+				Message: "Try and Success",
+			},
+		)
 		if err != nil {
-			log.Printf("UnaryEcho error: %v", err)
+			log.Printf("i:%d UnaryEcho error: %v", i, err)
 		}
-		log.Printf("UnaryEcho reply: %v", reply)
+		log.Printf("i:%d UnaryEcho reply: %v", i, reply)
+	}
+}
+
+func unaryClientInterceptor(failpercent int, failcodes string) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+
+		md := metadata.Pairs(
+			"failpercent", fmt.Sprintf("%d", failpercent),
+			"failcodes", failcodes,
+		)
+		ctxMD := metadata.NewOutgoingContext(ctx, md)
+
+		return invoker(ctxMD, method, req, reply, cc, opts...)
 	}
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -23,72 +23,221 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net"
-	"sync"
+	"strconv"
+	"strings"
+	"sync/atomic"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+
+	_ "unsafe"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50052, "port number")
+// unsafe for FastRandN
 
-type failingServer struct {
+// https://cs.opensource.google/go/go/+/master:src/runtime/stubs.go;l=151?q=FastRandN&ss=go%2Fgo
+// https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
+
+//go:linkname FastRandN runtime.fastrandn
+func FastRandN(n uint32) uint32
+
+const (
+	// https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
+	// We want a range between 1 and 16, so our maxCode is 15, cos we will +1
+	maxCode = 15
+)
+
+var (
+	success atomic.Uint64
+	fail    atomic.Uint64
+
+	errMissingMetadata = status.Errorf(codes.InvalidArgument, "missing metadata")
+)
+
+type echoServer struct {
 	pb.UnimplementedEchoServer
-	mu sync.Mutex
-
-	reqCounter uint
-	reqModulo  uint
 }
 
-// this method will fail reqModulo - 1 times RPCs and return status code Unavailable,
-// and succeeded RPC on reqModulo times.
-func (s *failingServer) maybeFailRequest() error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.reqCounter++
-	if (s.reqModulo > 0) && (s.reqCounter%s.reqModulo == 0) {
-		return nil
-	}
-
-	return status.Errorf(codes.Unavailable, "maybeFailRequest: failing it")
+func newEchoServer() (s echoServer) {
+	s = *new(echoServer)
+	return s
 }
 
-func (s *failingServer) UnaryEcho(_ context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	if err := s.maybeFailRequest(); err != nil {
-		log.Println("request failed count:", s.reqCounter)
-		return nil, err
-	}
+func (s echoServer) UnaryEcho(_ context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 
-	log.Println("request succeeded count:", s.reqCounter)
+	log.Println("request succeeded count:", success.Add(1))
+
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
+// https://pkg.go.dev/google.golang.org/grpc?utm_source=godoc#UnaryServerInterceptor
+func unaryServerInterceptor(failureRate int) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+
+		// https://grpc.io/docs/guides/metadata/
+		// https://github.com/grpc/grpc-go/blob/master/examples/features/metadata/server/main.go
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, errMissingMetadata
+		}
+
+		fp, errG := readFailPercent(failureRate, &md)
+		if errG != nil {
+			return nil, errG
+		}
+
+		fcs, errC := readFailCodes(&md)
+		if errC != nil {
+			return nil, errC
+		}
+
+		rp := FastRandN(100)
+		if rp < uint32(fp) {
+
+			f := fail.Add(1)
+
+			var code codes.Code
+
+			switch len(fcs) {
+			case 0:
+				code = anyRandomCode()
+			case 1:
+				code = fcs[0]
+			default:
+				code = returnAnySuppliedCode(&fcs)
+			}
+
+			log.Printf("request failed count:%d code:%s", f, code.String())
+
+			return nil, status.Errorf(code, "intercept failure code:%d rp:%d fail:%d", uint32(code), rp, f)
+		}
+
+		return handler(ctx, req)
+	}
+}
+
+// readFailPercent reads the "failpercent" percentage, including validation
+// percentage needs to be a integer between 0-100
+// e.g. failme = 10 ( 10% )
+// e.g. failme = 90 ( 90% )
+func readFailPercent(failureRate int, md *metadata.MD) (int, error) {
+
+	// metadata keys are always lower case
+	// https://github.com/grpc/grpc-go/blob/v1.68.0/metadata/metadata.go#L207
+	if t, ok := (*md)["failpercent"]; ok {
+		i, err := strconv.ParseInt(t[0], 0, 64)
+		if err != nil {
+			return 0, status.Error(codes.InvalidArgument, "failme ParseInt error")
+		}
+		errV := validateFailurePercent(int(i))
+		if errV != nil {
+			return 0, status.Error(codes.InvalidArgument, "failme validateFailurePercent error")
+		}
+		log.Printf("failme:%d from metadata:\n", i)
+
+		return int(i), nil
+	}
+	return failureRate, nil
+}
+
+// validateFailurePercent ensure the percentage is between 0-100 inclusive
+func validateFailurePercent(failureRate int) error {
+	if failureRate < 0 || failureRate > 100 {
+		return errors.New("invalid failureRate")
+	}
+	return nil
+}
+
+// readFailCodes returns a slice of codes.Code from the metadata "failcodes"
+// calls can include a single code, or a comma seperated list of codes
+// e.g. failcodes = 14 (unavailable)
+// e.g. failcodes = 10,12,14
+// valid codes: https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
+func readFailCodes(md *metadata.MD) (cs []codes.Code, errR error) {
+
+	if fc, ok := (*md)["failcodes"]; ok {
+
+		if !strings.Contains(fc[0], ",") {
+			c, err := strconv.ParseInt(fc[0], 0, 64)
+			if err != nil {
+				return cs, status.Error(codes.InvalidArgument, "failcodes ParseInt error")
+			}
+			if validateCodeUint32(uint32(c)) != nil {
+				return cs, status.Error(codes.InvalidArgument, "failcodes validate error")
+			}
+			cs = append(cs, codes.Code(uint32(c)))
+			return cs, nil
+		}
+
+		parts := strings.Split(fc[0], ",")
+		for i := 0; i < len(parts); i++ {
+			i, err := strconv.ParseInt(parts[i], 0, 64)
+			if err != nil {
+				return cs, status.Error(codes.InvalidArgument, "failcodes ParseInt error")
+			}
+			if validateCodeUint32(uint32(i)) != nil {
+				return cs, status.Error(codes.InvalidArgument, "failcodes validate error")
+			}
+			cs = append(cs, codes.Code(uint32(i)))
+		}
+		return cs, nil
+	}
+	return cs, nil
+}
+
+// validateCodeUint32 ensure the code is between 0-16 inclusive
+// code can't be < 0 because it's a uint32
+func validateCodeUint32(code uint32) error {
+	if code > 16 {
+		return errors.New("invalid code")
+	}
+	return nil
+}
+
+// anyRandomCode returns ANY random valid code ( 0-16 )
+// if the request metadata does not contain "failcodes"
+func anyRandomCode() (code codes.Code) {
+	return codes.Code(FastRandN(maxCode) + 1)
+}
+
+// returnAnySuppliedCode randomly selects from the
+// metadata supplied list of codes in "failcodes"
+func returnAnySuppliedCode(cs *[]codes.Code) (code codes.Code) {
+	return (*cs)[int(FastRandN(uint32(len(*cs))))]
+}
+
 func main() {
+
+	port := flag.Int("port", 50052, "port number")
+	address := fmt.Sprintf(":%v", *port)
+	failurePercent := flag.Int("failurePercent", 50, "failurePercent")
+
 	flag.Parse()
 
-	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
+
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Println("listen on address", address)
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(unaryServerInterceptor(*failurePercent)),
+	)
 
-	// Configure server to pass every fourth RPC;
-	// client is configured to make four attempts.
-	failingservice := &failingServer{
-		reqCounter: 0,
-		reqModulo:  4,
-	}
+	srv := newEchoServer()
 
-	pb.RegisterEchoServer(s, failingservice)
+	pb.RegisterEchoServer(s, srv)
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
